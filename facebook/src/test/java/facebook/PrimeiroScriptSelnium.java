cd5507c2a24caf6b62c23f115c906582eab97a95
Guilherme Dias Ferreira <guilhermedias.ferreira@hotmail.com>
2023-05-08 18:58:35 -0300

    preparando o projeto para rodar o selenium

---
+++ facebook/src/test/java/facebook/PrimeiroScriptSelnium.java
@@ -0,0 +1,25 @@
+package facebook;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PrimeiroScriptSelnium {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.get("https://www.google.com/");
+
+		driver.close();
+
+	}
+}
